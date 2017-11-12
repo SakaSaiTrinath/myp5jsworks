646e7f32fc604ac9a3ce0168c70e2ad5ea57c906
SakaSaiTrinath <sst.trinath@gmail.com>
2017-11-13 00:57:05 +0530

    Create stars.js

---
+++ chrome dino game/stars.js
@@ -0,0 +1,23 @@
+function Star() {
+	this.x = random(width);
+	this.y = random(200);
+	this.width = 5;
+	this.speed = 2;
+
+	this.display = function() {
+		fill(255);
+		ellipse(this.x,this.y,5,5);
+	}
+
+	this.update = function() {
+		this.x -= this.speed;
+	}
+
+	this.outOfScreen = function() {
+		if (this.x < -this.width) {
+	      return true;
+	    } else {
+	      return false;
+	   	}
+	}
+}
