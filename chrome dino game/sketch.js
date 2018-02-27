4160878fd8658e0e40a6b5b959e3616a01df3edd
Saka Sai Trinath <sst.trinath@gmail.com>
2018-02-27 17:16:49 +0530

    Update sketch.js

--- chrome dino game/sketch.js
+++ chrome dino game/sketch.js
@@ -1,12 +1,37 @@
 var start = false;
+var groundy = 350;
 var dino;
 var obs = [];
 var stars = [];
-var score = 0;
+var score = 0;  
+var hit;
+var buttonPress;
+var scoreReached;
+var speed=5;
+var gap=150;
+var pikachu;
+var obspic1; 
+var obspic2;
+var obspic3;
+//var birdpic;
+
+function preload() {
+	hit = loadSound("sounds/hit.mp3");
+	buttonPress = loadSound("sounds/button-press.mp3");
+	scoreReached = loadSound("sounds/score-reached.mp3"); 
+	pikachu = loadImage("images/pikachu4.png");
+	obspic1 = loadImage("images/obs1.png");
+	obspic2 = loadImage("images/obs2.png");
+	obspic3 = loadImage("images/obs3.png");
+	//birdpic = loadImage("images/birdpic.png");
+} 
+
 
 function setup() {
-	createCanvas(innerWidth,400);
-	dino = new Dinosaur();
+
+	let canvas = createCanvas(800,400);
+	canvas.position((innerWidth-width)/2,(innerHeight-height)/2);
+	dino = new Dinosaur(pikachu,groundy); 
 	obs.push(new Obs());
 	stars.push(new Star());
 }
@@ -18,27 +43,39 @@ function draw() {
   	//bottom ground line
   	strokeWeight(2);
   	stroke(255);
-	line(0,350,innerWidth,350);
+	line(0,groundy+2,innerWidth,groundy+2);
 
 	//score display
 	textSize(15);
 	strokeWeight(0);
-	score = floor(frameCount/2);
-	text("SCORE :  "+score,width-200,50);
+	stroke(255);
+	score = floor(frameCount/5)+9;
+	if(score%100==0)
+		scoreReached.play();
+	
+	text("SCORE :  "+score,width-200,50);  
 
 	//obstacle placement
 	for(var i=obs.length-1; i>=0; i--) {
 		obs[i].display();
-		obs[i].move(5);
+		obs[i].move();
+					
+		if(score%100==0) {
+			speed+=0.05;
+			obs[i].upspeed(speed);
+		}
 
 		if(obs[i].hits(dino)) {	//if hits game ends
-			console.log('HITS');
+			//console.log('HITS');
+			hit.play();
 			dino.display();
 
+			stroke(255);
+			fill(255);
 			textSize(50);
 			text("OOPS!!! GAME ENDED", width/2-250, height/2);
 			textSize(20);
-			text("Please Refresh the browser to PLAY again!",500,height/2+50);
+			text("Please Refresh the browser to PLAY again!",width/2-170,height/2+50);
 
 			for(var i=obs.length-1; i>=0; i--) {
 				obs[i].endfun();
@@ -57,6 +94,7 @@ function draw() {
 
 	dino.display();
 
+	//stars placement
 	for(var j=stars.length-1; j>=0; j--) {
 		stars[j].display();
 		stars[j].update();
@@ -66,18 +104,51 @@ function draw() {
 		}
 	}
 
+	
+
+	//---------------------------------------------------------------------------------------------------------
+	var obsx = width-random(gap);
+
+	if(score%200==0)
+		gap+=5;
 
 	if (frameCount % 80 == 0) {	//On every 80 frames one new obstacle adds
-	    obs.push(new Obs());
+
+    	if(frameCount % 200 == 0) {		//normal obstacle adds
+    		if (score>500) { 
+	    		var pos = [0,1];
+				var p = random(pos);
+				if(p == 0) {
+					 obs.push(new Obs(obspic1,obsx,speed,1,p,groundy));
+				} else { 
+					 obs.push(new Obs(obspic1,obsx,speed,1,p,groundy));
+				}
+	    	}
+		} else {			//normal obstacle adds
+			var pos = [0,1,2];			
+			var p = random(pos);
+
+			if(p == 0) {
+				obs.push(new Obs(obspic1,obsx,speed,0,0,groundy));				
+			} else if (p == 1) {
+				obs.push(new Obs(obspic2,obsx,speed,0,0,groundy));
+			} else {
+				obs.push(new Obs(obspic3,obsx,speed,0,0,groundy));
+			}
+			//obs.push(new Obs(obspic2,obsx,speed,0,0,groundy));	
+		}
+										
 	}
 
 	if(frameCount % 10 == 0) {	//On every 10 frames one new star adds
 		stars.push(new Star());
 	}
+
 }
 
 function keyPressed() {	//On pressing spacebar dino starts moves up
 	if(key == ' ') {
 		start = true;
+		buttonPress.play();
 	}
 }
